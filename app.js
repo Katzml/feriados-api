66ee19e02662a82fdad721e9eb2d00382d1ccb66
Sebastian ICP <sebastian.icp@hotmail.com>
2020-01-04 11:56:35 -0300

    second

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app= express();
 //importando json local de feriados
 const feriados = require('./assets/feriados2020.json');
 
+
+app.get('/',(req,res)=>{
+    res.send(`
+            <h1>Hola</h1>
+    
+    `)
+});
+
 app.get('/api/feriados',(req,res)=>{
     res.json(feriados);
 });
